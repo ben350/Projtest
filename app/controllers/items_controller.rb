c0b4d5327db4575e4d6d954b13a87032e8461237
Benji Ramirez <benjiramirez@Benjis-MacBook-Pro.local>
2015-05-06 14:21:10 -0700

    have cart working successfully

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,8 @@ class ItemsController < ApplicationController
   # GET /items/1
   # GET /items/1.json
   def show
+    @item = Item.find(params[:id])
+    @cart_action = @item.cart_action current_user.try :id
   end
 
   # GET /items/new
--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,14 @@ Rails.application.routes.draw do
   resources :movies, only: [:show, :index]
   resources :items, only: [:show, :index]
   resource :cart, only: [:show] do
-    put 'add/:movie_id', to: 'carts#add', as: :add_to
-    put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
+    put 'add/:item_id', to: 'carts#add', as: :add_to
+    put 'remove/:item_id', to: 'carts#remove', as: :remove_from
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'movies#index'
+  root 'items#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
